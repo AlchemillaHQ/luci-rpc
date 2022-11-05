77798e901d88752155b9b86a047a0a09f7bf08b4
hayzam <hayzam@gmail.com>
2022-11-06 03:52:11 +0530

    first commit

---
+++ example.js
@@ -0,0 +1,17 @@
+const { LUCI } = require('./lib/luci')
+
+async function main() {
+    const luci = new LUCI('https://192.168.1.2', 'root', 'x')
+
+    await luci.init()
+
+    console.log(await luci.getAll("network"))
+    console.log(await luci.get(["network", "@device[0]", "ports"]))
+
+    await luci.set(["network", "lan", "ipaddr", "192.168.1.1"])
+    console.log(await luci.getChangedSections())
+    console.log(await luci.getChanges())
+    console.log(await luci.commit("network"))
+}
+
+main()
---
+++ index.js
@@ -0,0 +1,5 @@
+const { LUCI } = require('./lib/luci')
+
+module.exports = {
+    LUCI
+}
