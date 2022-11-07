945ce28164801515ffbd27296e6f54902b309aa4
hayzam <hayzam@gmail.com>
2022-11-07 06:39:32 +0530

    feat: add autoUpdateToken function to keep refreshing tokens

--- example.js
+++ example.js
@@ -1,9 +1,12 @@
-const { LUCI } = require('./lib/luci')
+const { LUCI } = require('luci-rpc')
 
 async function main() {
     const luci = new LUCI('https://192.168.1.2', 'root', 'x')
 
     await luci.init()
+
+    let updateInterval = 1000 * 60 * 30
+    luci.autoUpdateToken(updateInterval);
 
     console.log(await luci.getAll("network"))
     console.log(await luci.get(["network", "@device[0]", "ports"]))
